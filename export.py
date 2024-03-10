5a803271f9f8348fa7eb162d453c1b8d6098c102
SCHWMAX <max.schwenzer@voith.com>
2024-03-10 09:21:48 +0100

    ONNX export non-maximum-suppression

--- export.py
+++ export.py
@@ -2,12 +2,15 @@ import argparse
 import sys
 import time
 import warnings
+from pathlib import Path
 
 sys.path.append('./')  # to run '$ python *.py' files in subdirectories
 
 import torch
 import torch.nn as nn
 from torch.utils.mobile_optimizer import optimize_for_mobile
+from onnxconverter_common import float16
+
 
 import models
 from models.experimental import attempt_load, End2End
@@ -15,6 +18,7 @@ from utils.activations import Hardswish, SiLU
 from utils.general import set_logging, check_img_size
 from utils.torch_utils import select_device
 from utils.add_nms import RegisterNMS
+from utils.debugging import print_debug_msg
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -35,6 +39,9 @@ if __name__ == '__main__':
     parser.add_argument('--fp16', action='store_true', help='CoreML FP16 half-precision export')
     parser.add_argument('--int8', action='store_true', help='CoreML INT8 quantization')
     opt = parser.parse_args()
+
+    print_debug_msg(opt, "INFO")
+
     opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
     opt.dynamic = opt.dynamic and not opt.end2end
     opt.dynamic = False if opt.dynamic_batch else opt.dynamic
@@ -70,67 +77,68 @@ if __name__ == '__main__':
         model.model[-1].include_nms = True
         y = None
 
-    # TorchScript export
-    try:
-        print('\nStarting TorchScript export with torch %s...' % torch.__version__)
-        f = opt.weights.replace('.pt', '.torchscript.pt')  # filename
-        ts = torch.jit.trace(model, img, strict=False)
-        ts.save(f)
-        print('TorchScript export success, saved as %s' % f)
-    except Exception as e:
-        print('TorchScript export failure: %s' % e)
+    # # TorchScript export
+    # try:
+    #     print(f"\nStarting TorchScript export with torch {torch.__version__} ...")
+    #     filename = Path(opt.weights).with_suffix(".torchscript.pt")  # filename
+    #     ts = torch.jit.trace(model, img, strict=False)
+    #     ts.save(filename)
+    #     print(f"TorchScript export success; saved as {filename.as_posix()}")
+    # except Exception as e:
+    #     print(f"TorchScript export failure: {e}")
 
-    # CoreML export
-    try:
-        import coremltools as ct
-
-        print('\nStarting CoreML export with coremltools %s...' % ct.__version__)
-        # convert model from torchscript and apply pixel scaling as per detect.py
-        ct_model = ct.convert(ts, inputs=[ct.ImageType('image', shape=img.shape, scale=1 / 255.0, bias=[0, 0, 0])])
-        bits, mode = (8, 'kmeans_lut') if opt.int8 else (16, 'linear') if opt.fp16 else (32, None)
-        if bits < 32:
-            if sys.platform.lower() == 'darwin':  # quantization only supported on macOS
-                with warnings.catch_warnings():
-                    warnings.filterwarnings("ignore", category=DeprecationWarning)  # suppress numpy==1.20 float warning
-                    ct_model = ct.models.neural_network.quantization_utils.quantize_weights(ct_model, bits, mode)
-            else:
-                print('quantization only supported on macOS, skipping...')
-
-        f = opt.weights.replace('.pt', '.mlmodel')  # filename
-        ct_model.save(f)
-        print('CoreML export success, saved as %s' % f)
-    except Exception as e:
-        print('CoreML export failure: %s' % e)
+    # # CoreML export
+    # try:
+    #     import coremltools as ct
+    #
+    #     print(f"\nStarting CoreML export with coremltools {ct.__version__} ...")
+    #     # convert model from torchscript and apply pixel scaling as per detect.py
+    #     ct_model = ct.convert(ts, inputs=[ct.ImageType('image', shape=img.shape, scale=1 / 255.0, bias=[0, 0, 0])])
+    #     bits, mode = (8, 'kmeans_lut') if opt.int8 else (16, 'linear') if opt.fp16 else (32, None)
+    #     if bits < 32:
+    #         if sys.platform.lower() == 'darwin':  # quantization only supported on macOS
+    #             with warnings.catch_warnings():
+    #                 warnings.filterwarnings("ignore", category=DeprecationWarning)  # suppress numpy==1.20 float warning
+    #                 ct_model = ct.models.neural_network.quantization_utils.quantize_weights(ct_model, bits, mode)
+    #         else:
+    #             print('quantization only supported on macOS, skipping ...')
+    #
+    #     filename = Path(opt.weights).with_suffix(".mlmodel")  # filename
+    #     ct_model.save(filename)
+    #     print(f"CoreML export success; saved as {filename}")
+    # except Exception as e:
+    #     print(f"CoreML export failure: {e}")
                      
-    # TorchScript-Lite export
-    try:
-        print('\nStarting TorchScript-Lite export with torch %s...' % torch.__version__)
-        f = opt.weights.replace('.pt', '.torchscript.ptl')  # filename
-        tsl = torch.jit.trace(model, img, strict=False)
-        tsl = optimize_for_mobile(tsl)
-        tsl._save_for_lite_interpreter(f)
-        print('TorchScript-Lite export success, saved as %s' % f)
-    except Exception as e:
-        print('TorchScript-Lite export failure: %s' % e)
+    # # TorchScript-Lite export
+    # try:
+    #     print("\nStarting TorchScript-Lite export with torch {torch.__version__} ...")
+    #     filename = Path(opt.weights).with_suffix(".torchscript.ptl")  # filename
+    #     tsl = torch.jit.trace(model, img, strict=False)
+    #     tsl = optimize_for_mobile(tsl)
+    #     tsl._save_for_lite_interpreter(filename)
+    #     print(f"TorchScript-Lite export success; saved as {filename}")
+    # except Exception as e:
+    #     print(f"TorchScript-Lite export failure: {e}")
 
     # ONNX export
     try:
         import onnx
 
-        print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
-        f = opt.weights.replace('.pt', '.onnx')  # filename
+        print(f"\nStarting ONNX export with onnx {onnx.__version__} ...")
+        filename = Path(opt.weights).with_suffix(".onnx")  # filename
         model.eval()
         output_names = ['classes', 'boxes'] if y is None else ['output']
         dynamic_axes = None
         if opt.dynamic:
-            dynamic_axes = {'images': {0: 'batch', 2: 'height', 3: 'width'},  # size(1,3,640,640)
-             'output': {0: 'batch', 2: 'y', 3: 'x'}}
+            dynamic_axes = {
+                'images': {0: 'batch', 2: 'height', 3: 'width'},  # size(1, 3, 640, 640)
+                'output': {0: 'batch', 2: 'y', 3: 'x'}
+            }
         if opt.dynamic_batch:
             opt.batch_size = 'batch'
             dynamic_axes = {
-                'images': {
-                    0: 'batch',
-                }, }
+                'images': {0: 'batch'},
+            }
             if opt.end2end and opt.max_wh is None:
                 output_axes = {
                     'num_dets': {0: 'batch'},
@@ -145,9 +153,9 @@ if __name__ == '__main__':
             dynamic_axes.update(output_axes)
         if opt.grid:
             if opt.end2end:
-                print('\nStarting export end2end onnx model for %s...' % 'TensorRT' if opt.max_wh is None else 'onnxruntime')
-                model = End2End(model,opt.topk_all,opt.iou_thres,opt.conf_thres,opt.max_wh,device,len(labels))
-                if opt.end2end and opt.max_wh is None:
+                print(f"\nStarting export end2end onnx model for {'TensorRT' if opt.max_wh is None else 'onnxruntime'} ...")
+                model = End2End(model, opt.topk_all, opt.iou_thres, opt.conf_thres, opt.max_wh, device, len(labels))
+                if opt.max_wh is None:
                     output_names = ['num_dets', 'det_boxes', 'det_scores', 'det_classes']
                     shapes = [opt.batch_size, 1, opt.batch_size, opt.topk_all, 4,
                               opt.batch_size, opt.topk_all, opt.batch_size, opt.topk_all]
@@ -156,20 +164,28 @@ if __name__ == '__main__':
             else:
                 model.model[-1].concat = True
 
-        torch.onnx.export(model, img, f, verbose=False, opset_version=12, input_names=['images'],
-                          output_names=output_names,
-                          dynamic_axes=dynamic_axes)
+        torch.onnx.export(
+            model,
+            img,
+            filename.as_posix(),
+            verbose=False,
+            opset_version=15,
+            input_names=['images'],
+            output_names=output_names,
+            dynamic_axes=dynamic_axes
+        )
+        # output (? x 7) [batch_index, x1, y1, x2?, y2?, class_id, conf]
 
         # Checks
-        onnx_model = onnx.load(f)  # load onnx model
+        onnx_model = onnx.load(filename)  # load onnx model
         onnx.checker.check_model(onnx_model)  # check onnx model
 
         if opt.end2end and opt.max_wh is None:
             for i in onnx_model.graph.output:
                 for j in i.type.tensor_type.shape.dim:
-                    j.dim_param = str(shapes.pop(0))
+                    j.dim_param = str(shapes.pop(0)) # FIXME: if --grid is not set, this can be reached without "shapes" being defined
 
-        # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
+        # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human-readable model
 
         # # Metadata
         # d = {'stride': int(max(model.stride))}
@@ -182,24 +198,37 @@ if __name__ == '__main__':
             try:
                 import onnxsim
 
-                print('\nStarting to simplify ONNX...')
+                print('\nStarting to simplify ONNX ...')
                 onnx_model, check = onnxsim.simplify(onnx_model)
                 assert check, 'assert check failed'
             except Exception as e:
                 print(f'Simplifier failure: {e}')
 
-        # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
-        onnx.save(onnx_model,f)
-        print('ONNX export success, saved as %s' % f)
+        # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human-readable model
+        onnx.save(onnx_model, filename)
+        print(f"ONNX export success; saved as {filename}")
 
         if opt.include_nms:
-            print('Registering NMS plugin for ONNX...')
-            mo = RegisterNMS(f)
+            print('Registering NMS plugin for ONNX ...')
+            mo = RegisterNMS(filename.as_posix())
+            print('Register NMS ...')
             mo.register_nms()
-            mo.save(f)
+            print('Save extended model ...')
+            mo.save(filename)
+
+        if opt.fp16:
+            print("Converting ONNX model to fp16 precision:")
+            print("Loading model ...")
+            onnx_model = onnx.load(filename)  # load onnx model
+            print("Checking model ...")
+            onnx.checker.check_model(onnx_model)  # check onnx model
+            print("Converting graph to fp16 ...")
+            model_fp16 = float16.convert_float_to_float16(onnx_model)
+            print("Saving model ...")
+            onnx.save(model_fp16, filename.with_name(f"{filename.name}_fp16"))
 
     except Exception as e:
-        print('ONNX export failure: %s' % e)
+        print(f"ONNX export failure: {e}")
 
     # Finish
-    print('\nExport complete (%.2fs). Visualize with https://github.com/lutzroeder/netron.' % (time.time() - t))
+    print(f"\nExport complete ({(time.time() - t):.2}s). Visualize with https://github.com/lutzroeder/netron.")
--- utils/add_nms.py
+++ utils/add_nms.py
@@ -1,155 +1,140 @@
 import numpy as np
 import onnx
-from onnx import shape_inference
-try:
-    import onnx_graphsurgeon as gs
-except Exception as e:
-    print('Import onnx_graphsurgeon failure: %s' % e)
+from onnx import helper, shape_inference
 
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
+
 class RegisterNMS(object):
-    def __init__(
-        self,
-        onnx_model_path: str,
-        precision: str = "fp32",
-    ):
+    model = None
 
-        self.graph = gs.import_onnx(onnx.load(onnx_model_path))
-        assert self.graph
-        LOGGER.info("ONNX graph created successfully")
-        # Fold constants via ONNX-GS that PyTorch2ONNX may have missed
-        self.graph.fold_constants()
-        self.precision = precision
+    def __init__(self, onnx_model_path: str, precision: str = "fp32"):
+        self.model_path = onnx_model_path
         self.batch_size = 1
-    def infer(self):
-        """
-        Sanitize the graph by cleaning any unconnected nodes, do a topological resort,
-        and fold constant inputs values. When possible, run shape inference on the
-        ONNX graph to determine tensor shapes.
-        """
-        for _ in range(3):
-            count_before = len(self.graph.nodes)
 
-            self.graph.cleanup().toposort()
-            try:
-                for node in self.graph.nodes:
-                    for o in node.outputs:
-                        o.shape = None
-                model = gs.export_onnx(self.graph)
-                model = shape_inference.infer_shapes(model)
-                self.graph = gs.import_onnx(model)
-            except Exception as e:
-                LOGGER.info(f"Shape inference could not be performed at this time:\n{e}")
-            try:
-                self.graph.fold_constants(fold_shapes=True)
-            except TypeError as e:
-                LOGGER.error(
-                    "This version of ONNX GraphSurgeon does not support folding shapes, "
-                    f"please upgrade your onnx_graphsurgeon module. Error:\n{e}"
-                )
-                raise
-
-            count_after = len(self.graph.nodes)
-            if count_before == count_after:
-                # No new folding occurred in this iteration, so we can stop for now.
-                break
 
     def save(self, output_path):
-        """
-        Save the ONNX model to the given location.
-        Args:
-            output_path: Path pointing to the location where to write
-                out the updated ONNX model.
-        """
-        self.graph.cleanup().toposort()
-        model = gs.export_onnx(self.graph)
-        onnx.save(model, output_path)
-        LOGGER.info(f"Saved ONNX model to {output_path}")
+        if self.model is not None:
+            onnx.save(self.model, output_path)
+            LOGGER.info(f"Saved ONNX model to {output_path}")
+        else:
+            raise Exception("Model was not registered yet. Please call register_model() first.")
 
     def register_nms(
-        self,
-        *,
-        score_thresh: float = 0.25,
-        nms_thresh: float = 0.45,
-        detections_per_img: int = 100,
+            self,
+            score_thresh: float = 0.25,
+            nms_iou_thresh: float = 0.45,
+            detections_per_img: int = 100
     ):
-        """
-        Register the ``EfficientNMS_TRT`` plugin node.
-        NMS expects these shapes for its input tensors:
-            - box_net: [batch_size, number_boxes, 4]
-            - class_net: [batch_size, number_boxes, number_labels]
-        Args:
-            score_thresh (float): The scalar threshold for score (low scoring boxes are removed).
-            nms_thresh (float): The scalar threshold for IOU (new boxes that have high IOU
-                overlap with previously selected boxes are removed).
-            detections_per_img (int): Number of best detections to keep after NMS.
-        """
+        opset_version_act = onnx.defs.onnx_opset_version()
+        if opset_version_act < 13:
+            raise Exception(
+                f"Please upgrade to ONNX opset version 13 or higher to use NonMaxSuppression. "
+                f"Current version is {opset_version_act}."
+            )
+        onnx_model = onnx.load(self.model_path)
+        # model output = non-max-suppression input. Shape: (-1 x 7) [batch_index, x1, y1, x2?, y2?, class_id, conf]
 
-        self.infer()
-        # Find the concat node at the end of the network
-        op_inputs = self.graph.outputs
-        op = "EfficientNMS_TRT"
-        attrs = {
-            "plugin_version": "1",
-            "background_class": -1,  # no background class
-            "max_output_boxes": detections_per_img,
-            "score_threshold": score_thresh,
-            "iou_threshold": nms_thresh,
-            "score_activation": False,
-            "box_coding": 0,
-        }
+        # Get the output names
+        output_names = [output.name for output in onnx_model.graph.output]
 
-        if self.precision == "fp32":
-            dtype_output = np.float32
-        elif self.precision == "fp16":
-            dtype_output = np.float16
-        else:
-            raise NotImplementedError(f"Currently not supports precision: {self.precision}")
-
-        # NMS Outputs
-        output_num_detections = gs.Variable(
-            name="num_dets",
-            dtype=np.int32,
-            shape=[self.batch_size, 1],
-        )  # A scalar indicating the number of valid detections per batch image.
-        output_boxes = gs.Variable(
-            name="det_boxes",
-            dtype=dtype_output,
-            shape=[self.batch_size, detections_per_img, 4],
-        )
-        output_scores = gs.Variable(
-            name="det_scores",
-            dtype=dtype_output,
-            shape=[self.batch_size, detections_per_img],
-        )
-        output_labels = gs.Variable(
-            name="det_classes",
-            dtype=np.int32,
-            shape=[self.batch_size, detections_per_img],
+        # Slice the tensors
+        # ---- bounding box
+        bboxes_start_constant = helper.make_node(
+            'Constant',
+            inputs=[],
+            outputs=['bboxes_start_constant'],
+            value=helper.make_tensor(name="starts_tensor", data_type=onnx.TensorProto.INT32, dims=[1], vals=[1])
         )
 
-        op_outputs = [output_num_detections, output_boxes, output_scores, output_labels]
+        bboxes_end_constant = helper.make_node(
+            'Constant',
+            inputs=[],
+            outputs=['bboxes_end_constant'],
+            value=helper.make_tensor(name="ends_tensor", data_type=onnx.TensorProto.INT32, dims=[1], vals=[4])
+        )
 
-        # Create the NMS Plugin node with the selected inputs. The outputs of the node will also
-        # become the final outputs of the graph.
-        self.graph.layer(op=op, name="batched_nms", inputs=op_inputs, outputs=op_outputs, attrs=attrs)
-        LOGGER.info(f"Created NMS plugin '{op}' with attributes: {attrs}")
+        # Create a Slice node to extract elements from the first to the fourth column
+        bboxes = helper.make_node(
+            'Slice',  # Node type
+            inputs=[output_names[0], 'bboxes_start_constant', 'bboxes_end_constant'],  # Input tensors
+            outputs=['bboxes'],  # Output tensor name
+        )
+        # Add the Slice node to the graph
+        print("extend node to slice box coordinates ...")
+        onnx_model.graph.node.extend([bboxes_start_constant, bboxes_end_constant, bboxes])
+        onnx.checker.check_model(onnx_model)  # check onnx model
 
-        self.graph.outputs = op_outputs
+        # ---- scores
+        scores_start_constant = helper.make_node(
+            'Constant',
+            inputs=[],
+            outputs=['scores_start_constant'],
+            value=helper.make_tensor(name="starts_tensor", data_type=onnx.TensorProto.INT32, dims=[1], vals=[5])
+        )
 
-        self.infer()
+        scores_end_constant = helper.make_node(
+            'Constant',
+            inputs=[],
+            outputs=['scores_end_constant'],
+            value=helper.make_tensor(name="ends_tensor", data_type=onnx.TensorProto.INT32, dims=[1], vals=[6])
+        )
 
-    def save(self, output_path):
-        """
-        Save the ONNX model to the given location.
-        Args:
-            output_path: Path pointing to the location where to write
-                out the updated ONNX model.
-        """
-        self.graph.cleanup().toposort()
-        model = gs.export_onnx(self.graph)
-        onnx.save(model, output_path)
-        LOGGER.info(f"Saved ONNX model to {output_path}")
+        # Create a Slice node to extract elements from the first to the fourth column
+        scores = helper.make_node(
+            'Slice',  # Node type
+            inputs=[output_names[0], 'scores_start_constant', 'scores_end_constant'],  # Input tensors
+            outputs=['scores'],  # Output tensor name
+        )
+
+        # Add the Slice node to the graph
+        print("extend node to slice score vector ...")
+        onnx_model.graph.node.extend([scores_start_constant, scores_end_constant, scores])
+        onnx.checker.check_model(onnx_model)  # check onnx model
+
+        # ---- non-maximum-suppression
+        # attributes as constants
+        simple_constants = [
+            # Specify box format: 0 = [x1, y1, x2, y2] 1 = [x_center, y_center, width, height]
+            ("center_point_box", 0, onnx.TensorProto.INT32),
+            # Maximum number of output boxes to keep per class
+            ("max_output_boxes_per_class", detections_per_img, onnx.TensorProto.INT32),
+            # IoU threshold for NMS
+            ("iou_threshold", nms_iou_thresh, onnx.TensorProto.FLOAT)
+        ]
+
+        for ky, vl, ty in simple_constants:
+            constant = onnx.helper.make_tensor(ky, ty, [1], [vl])
+            onnx_model.graph.initializer.append(constant)
+
+        # create node with non-maximum-suppression
+        nms_node = helper.make_node(
+            op_type="NonMaxSuppression",
+            inputs=["bboxes", "scores"] + [el[0] for el in simple_constants],
+            outputs=["selected_indices"],
+            name="nms_node",
+        )
+
+        # Add the NMS node to the model
+        print("append node with nms ...")
+        onnx_model.graph.node.append(nms_node)
+        # onnx_model.graph.output.extend(op_outputs)
+        onnx.checker.check_model(onnx_model)  # check onnx model
+
+        print("infer shape ...")
+        # Infer shapes after modifying the graph
+        inferred_model = shape_inference.infer_shapes(onnx_model)
+
+        # Save the updated model
+        # self.save(model, "updated_model.onnx")
+        onnx.checker.check_model(inferred_model)  # check onnx model
+
+        # Store the updated model
+        self.model = inferred_model
+
+        LOGGER.info(f"Created NMS plugin 'NonMaxSuppression'.")
+
+        return inferred_model
+
